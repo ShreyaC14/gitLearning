ece44c16b68a49f041de6633d073d2aaaa5b0911
Shreya Choudhury <choudhuryshreya276@gmail.com>
2023-08-24 09:07:25 +0530

    initial commit

--- sum1.java
+++ sum1.java
@@ -4,6 +4,6 @@ public class sum1 {
     public static void main(String[] args){
         int a = 5;
         int b = 6;
-        System.out.println(a+b);
+        System.out.println("The sum of two num is :"+a+b);
     } 
 }
