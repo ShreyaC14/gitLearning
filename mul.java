20e866b74177685b7be1cc6641f93c65fed9a154
Shreya Choudhury <choudhuryshreya276@gmail.com>
2023-08-26 15:41:29 +0530

    mul committed

--- mul.java
+++ mul.java
@@ -1,5 +1,9 @@
 package gitLearning;
 
 public class mul {
-    
+    public static void main(String[] args){
+        int a = 5;
+        int b = 6;
+        System.out.println("The mul of two num is :"+a*b);
+    } 
 }
