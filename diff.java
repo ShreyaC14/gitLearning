50e1bc996837d12684493a925419480a9fead143
Shreya Choudhury <choudhuryshreya276@gmail.com>
2023-08-26 16:10:51 +0530

    Skipped staging area and fixed

--- diff.java
+++ diff.java
@@ -1,5 +1,9 @@
 package gitLearning;
 
 public class diff {
-    
+    public static void main(String[] args){
+        int a = 5;
+        int b = 6;
+        System.out.println("The remainder of two num is :"+a%b);
+    } 
 }
