4ecf7316b14c42d0488fbc8d2a1028de7d7c6c8b
Shreya Choudhury <choudhuryshreya276@gmail.com>
2023-08-26 17:16:14 +0530

    added multiply.java

--- multiply.java
+++ multiply.java
@@ -1,9 +1,9 @@
 package gitLearning;
 
-public class mul {
+public class multiply {
     public static void main(String[] args){
         int a = 5;
         int b = 6;
-        System.out.println("The mul of two num is :"+a*b);
+        System.out.println("The sum of two num is :"+(a*b));
     } 
 }
